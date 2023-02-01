9f8f52d6aad6f5b28b3d9cae7b86e618264cbae5
Nadir-KT <65030168+Nadir-KT@users.noreply.github.com>
2023-02-01 12:43:35 +0530

    redux

--- Project0/index.js
+++ Project0/index.js
@@ -8,9 +8,10 @@ import Flexbox from './src/flexbox';
 import Webview from './src/Webview';
 import Register from './src/Register';
 import Login from './src/Login';
+import State from './src/Fun/State';
 import State_Effect from './src/Fun/State_Effect';
-import Contest from './src/Fun/Contest'
-import Reducer from './src/Fun/Reducer'
+import Contest from './src/Fun/Contest';
+import Reducer from './src/Fun/Reducer';
 import {name as appName} from './app.json'; // import app.json file in same folder// take name mentioned in app.json as appName
 
-AppRegistry.registerComponent(appName, () => Reducer); // Select main source code to run & fletch appName to it.
+AppRegistry.registerComponent(appName, () => State); // Select main source code to run & fletch appName to it.
