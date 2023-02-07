abab3d943d23c5f69e7e6a7adad36351841898ae
Nadir-KT <65030168+Nadir-KT@users.noreply.github.com>
2023-02-07 12:58:29 +0530

    UI

--- MainProject/index.js
+++ MainProject/index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import Drop from './src/UI/Drop'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Drop);
