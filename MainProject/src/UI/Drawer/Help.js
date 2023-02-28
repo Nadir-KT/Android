f5643bed765249c97adbfc3cc48c064f231e8e52
Nadir-KT <65030168+Nadir-KT@users.noreply.github.com>
2023-02-28 13:54:15 +0530

    main

--- MainProject/src/UI/Drawer/Help.js
+++ MainProject/src/UI/Drawer/Help.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, useState } from 'react';
+import { TextInput, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 
-export default class Help extends Component 
-{
-    render(){return(
-        <View style={styles.container}>
-            <Text style={styles.text}>Hi this is Help page</Text>
+export default function Help ({ navigation }) {
+    const [data] = useState([
+        { name: 'Biology' },
+        { name: 'Physics' },
+        { name: 'Chemistry' },
+        { name: 'Technology' },
+        { name: 'Engineering' },
+        { name: 'Mathematics' },
+    ]);
+    return (
+        <View style={{  }}>
+            <TextInput style={{ width: 311, height: 51, marginTop: 32, backgroundColor: '#062E40', borderColor: '#007345', borderWidth: 1, borderRadius: 8, fontSize: 16 }}
+          placeholder='Full name' placeholderTextColor={'#446270'}></TextInput>
         </View>
-    )}
+    )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#123'
-    },
-    text: {fontSize: 24, fontWeight: 'bold', color: 'white'}
-})
